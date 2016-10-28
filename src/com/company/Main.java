558a4e87ad8e17ca002ae0b81ca5a9e7c3bdf55a
KAZI-TANI LAMIA <lamiakazitani5@gmail.com>
2016-10-28 04:19:34 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,8 +17,8 @@ public class Main {
         System.out.println(" Le point C mes coordonees sont : "+c);
         System.out.println("  \n les images de A,B,C =" );
         System.out.println(" l'image de A est : "+a.toString());
-        System.out.println(" l'image de B est : "+b.imagePoint());
-        System.out.println(" l'image de C est : "+c.imagePoint());
+        System.out.println(" l'image de B est : "+b.SymPoint());
+        System.out.println(" l'image de C est : "+c.SymgePoint());
 
 
 
@@ -29,19 +29,19 @@ public class Main {
         System.out.println(" Le Segment BC =  " +bc.Dis());
 
         System.out.println("-- \n Creation de l'image du Segment BC par rapport a l'axe des ordonnées  --");
-        System.out.println(" l'image de BC et CB = " +b.imagePoint()+c.imagePoint());
+        System.out.println(" l'image de BC et CB = " +b.SymPoint()+c.SymPoint());
         System.out.println("      -------------------------------------------------------------");
 
         Point O=new Point();
         int r=4;
         Cercle C=new Cercle(O,r);
-        System.out.println(" ---  le cercle C : \n Centre O "+O.toString()+" \n Rayon r = "+r+"\n perimetre ="+C.per()+"\n son image C-1 "+O.imagePoint()+"\n Rayon r="+r);
+        System.out.println(" ---  le cercle C : \n Centre O "+O.toString()+" \n Rayon r = "+r+"\n perimetre ="+C.per()+"\n son image C-1 "+O.SymPoint()+"\n Rayon r="+r);
 
 
         Point O1=new Point(6);
         int r1=9;
         Cercle C1=new Cercle(O1,r1);
-        System.out.println(" --- le cercle C1 : \n Centre O1 "+O1.toString()+" \n Rayon r1="+r1+"\n perimetre ="+C1.per()+" \n son image C1-1 "+O1.imagePoint()+"\n Rayon r1="+r1);
+        System.out.println(" --- le cercle C1 : \n Centre O1 "+O1.toString()+" \n Rayon r1="+r1+"\n perimetre ="+C1.per()+" \n son image C1-1 "+O1.SymPoint()+"\n Rayon r1="+r1);
 
 
 
